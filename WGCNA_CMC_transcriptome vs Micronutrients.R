e56221a3f27c27253546587a1541a6c90e1db9f9
Shubhankar Pawar <137198508+Shubhankar2697@users.noreply.github.com>
2025-01-03 16:19:17 +0530

    Update WGCNA_CMC_transcriptome vs Micronutrients.R

--- WGCNA_CMC_transcriptome vs Micronutrients.R
+++ WGCNA_CMC_transcriptome vs Micronutrients.R
@@ -1,16 +1,20 @@
+## Author: Shubhankar A Pawar. Contact: shubhankarpawar3@gmail.com
+
+
+
+
+
+getwd()
+setwd("/B12_WGCNA/nutshell/")
+gene1 <-read.csv("vst_no__batch_correction.csv")
+
 library(tibble)
 library(WGCNA)
 options(stringsAsFactors = FALSE)
-getwd()
-setwd("C:/Priya")
-gene1 <-read.csv("neonatal_expr_data_yy.csv")
-#read the clinical feature data
-#take a quick look on what is the dataset.
-dim(gene1)
-names(gene1)
-#fix(gene1)
-#fix(gene2)
-gene1<-column_to_rownames(gene1, var = "gene_symbol")
+
+dim(gene1) #read the clinical feature data
+names(gene1) #take a quick look on what is the dataset.
+
 
 t_gene1<-t(as.data.frame(gene1))
 rownames(t_gene1)<-sub("*\\X", "", rownames(t_gene1))
@@ -28,30 +32,30 @@ par(cex = 0.6);
 par(mar = c(0,4,2,0))
 plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
      cex.axis = 1.5, cex.main = 2)
+
 # Plot a line to show the cut
-abline(h = 2e+07, col = "red");
+abline(h = 140, col = "red");
 # Determine cluster under the line
-clust = cutreeStatic(sampleTree, cutHeight =2e+07 , minSize = 10)
+clust = cutreeStatic(sampleTree, cutHeight =140 , minSize = 10)
 table(clust)
 # clust 1 contains the samples we want to keep.
 keepSamples = (clust==1)
 datExpr = t_gene1[keepSamples, ]
 nGenes = ncol(datExpr)
 nSamples = nrow(datExpr)
-
-#loading clinical cluster data
-traitData = read.csv("B12_79.csv");
+dim(datExpr)
+#write.csv(datExpr, "expr_out_vst.csv")
+traitData = read.csv("clin_var.csv");#loading clinical cluster data
 dim(traitData)
 names(traitData)
 Wpheno<-column_to_rownames(traitData, var = "child_no")
 Wpheno
-
 # Form a data frame analogous to expression data that will hold the clinical traits.
 Samples<-rownames(datExpr)
-view(Samples)
+#view(Samples)
 rownames(datExpr)
 traitRows = match(Samples,rownames(Wpheno))
-view(traitRows)
+#view(traitRows)
 datTraits = Wpheno[traitRows,]
 collectGarbage();
 
@@ -65,13 +69,18 @@ plotDendroAndColors(sampleTree2, traitColors,
                     groupLabels = names(Wpheno),
                     main = "Sample dendrogram and trait heatmap")
 
-save(datExpr, datTraits, file = "cordEXP.RData")
-lnames = load(file ="cordEXP.RData")
+save(datExpr, datTraits, file = "cordEXP_vst_nb_new.RData")
+#--------------
+
+
+lnames = load(file ="cordEXP_vst_nb_new.RData")
 lnames
-# Choose a set of soft-thresholding powers
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
 # Call the network topology analysis function
-sft = pickSoftThreshold(datExpr, powerVector = powers, networkType = "signed", verbose = 5)
+sft = pickSoftThreshold(datExpr, powerVector = powers, 
+                        networkType = "signed", 
+                        enableWGCNAThreads(nThreads = 15), 
+                        verbose = 5)
 # Plot the results:
 sizeGrWindow(9, 5)
 par(mfrow = c(1,2));
@@ -83,19 +92,25 @@ plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
 text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
      labels=powers,cex=cex1,col="red");
 # this line corresponds to using an R^2 cut-off of h
-abline(h=0.90,col="red")
+abline(h=0.8,col="red")
 # Mean connectivity as a function of the soft-thresholding power
 plot(sft$fitIndices[,1], sft$fitIndices[,5],
      xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
      main = paste("Mean connectivity"))
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
+
+
+
+cor<-WGCNA::cor
 net = blockwiseModules(datExpr, power = 7, networkType = "signed",
-                       TOMType = "signed", minModuleSize = 30,
+                       TOMType = "signed", minModuleSize = 100,
                        reassignThreshold = 0, mergeCutHeight = 0.25,
                        numericLabels = TRUE, pamRespectsDendro = FALSE,
                        saveTOMs = TRUE,
-                       saveTOMFileBase = "Cord B12 expr",
-                       verbose = 3)
+                       saveTOMFileBase = "Cord_varriant_power7_expr_vst",
+                       verbose = 3,
+                       nThreads = 15)
+
 # open a graphics window
 sizeGrWindow(12, 9)
 # Convert labels to colors for plotting
@@ -110,9 +125,10 @@ moduleColors = labels2colors(net$colors)
 MEs = net$MEs;
 geneTree = net$dendrograms[[1]];
 save(MEs, moduleLabels, moduleColors, geneTree,
-     file = "Signed_7.RData")
+     file = "Signed_7_vst_nb.RData")
+
 # Load network data saved in the second part.
-vnames = load(file = "Signed_7.RData");
+vnames = load(file = "Signed_7_vst_nb.RData");
 vnames
 # Define numbers of genes and samples
 nGenes = ncol(datExpr);
@@ -127,7 +143,9 @@ sizeGrWindow(10,6)
 textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
                    signif(moduleTraitPvalue, 1), ")", sep = "");
 dim(textMatrix) = dim(moduleTraitCor)
-par(mar = c(8, 15,6, 6));
+
+# Adjust margins and text size
+par(mar = c(8, 10,3, 5));
 # Display the correlation values within a heatmap plot
 labeledHeatmap(Matrix = moduleTraitCor,
                xLabels = names(datTraits),
@@ -140,8 +158,9 @@ labeledHeatmap(Matrix = moduleTraitCor,
                cex.text = 0.5,
                zlim = c(-1,1),
                main = paste("Module-trait relationships"))
-#Define variable weight contaning the weight column of datTrait
-B12 = as.data.frame(datTraits$cord.blood..B12.levels);
+##################################################################
+
+B12 = as.data.frame(datTraits$Cord.blood..B12)
 names(B12) = "B12"
 # names (colors) of the modules
 modNames = substring(names(MEs), 3)
@@ -151,23 +170,8 @@ geneTraitSignificance = as.data.frame(cor(datExpr, B12, use = "p"));
 GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples));
 names(geneTraitSignificance) = paste("GS.", names(B12), sep="");
 names(GSPvalue) = paste("p.GS.", names(B12), sep="");
-
-
-########MM-GS for yellow  module which is positively associated with B12 levels########
-module = "yellow"
-column = match(module, modNames);
-moduleGenes = moduleColors==module;
-sizeGrWindow(7, 7);
-par(mfrow = c(1,1));
-verboseScatterplot(abs(geneModuleMembership[moduleGenes, column]),
-                   
-                   abs(geneTraitSignificance[moduleGenes, 1]),
-                   xlab = paste("Module Membership in", module, "module"),
-                   ylab = "Gene significance for B12 levels",
-                   main = paste("Module membership vs. gene significance\n"),
-                   cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, col = module)
-
-########MM-GS for turquoise module which is positively associated with B12 levels#######
+#########   MM-GS ####### B12 associated clusters
+# for power 14)
 module = "turquoise"
 column = match(module, modNames);
 moduleGenes = moduleColors==module;
@@ -182,26 +186,4 @@ verboseScatterplot(abs(geneModuleMembership[moduleGenes, column]),
                    cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, col = module)
 
 
-###########MM-GS for brown module which is negatively associated with B12 levels###
-module = "brown"
-column = match(module, modNames);
-moduleGenes = moduleColors==module;
-sizeGrWindow(7, 7);
-par(mfrow = c(1,1));
-verboseScatterplot(abs(geneModuleMembership[moduleGenes, column]),
-                   
-                   abs(geneTraitSignificance[moduleGenes, 1]),
-                   xlab = paste("Module Membership in", module, "module"),
-                   ylab = "Gene significance for B12 levels",
-                   main = paste("Module membership vs. gene significance\n"),
-                   cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, col = module)
 
-############# To extract modules associated with cord blood B12 levels##########
-yellow = colnames(datExpr)[moduleColors=="yellow"]
-write.csv(yellow, file = "Yellow_b12_signed.csv")
-
-turquoise = colnames(datExpr)[moduleColors=="turquoise"]
-write.csv(turquoise, file = "Turquoise_b12_signed.csv")
-
-brown = colnames(datExpr)[moduleColors=="brown"]
-write.csv(brown, file = "Brown_B12_signed.csv")
